bb9bd3f0eedcc27f210eb0f6488aca6ad537a264
Matous-TUL <matymartinek@seznam.cz>
2026-04-14 14:31:05 +0200

    Model for ExchangeRate

---
+++ src/main/java/model/ExchangeRateResponse.java
@@ -0,0 +1,12 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
+public class ExchangeRateResponse {
+    private Map<String, Double> rates;
+}
