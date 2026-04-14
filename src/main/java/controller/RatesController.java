790e1d8624b883ee2e763c973afd03fa46d6e74e
Matous-TUL <matymartinek@seznam.cz>
2026-04-14 18:44:24 +0200

    /rates

---
+++ src/main/java/controller/RatesController.java
@@ -0,0 +1,42 @@
+package controller;
+
+import model.ExchangeRateResponse;
+import model.ExchangeRateResponseDate;
+import model.UserSettings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import service.CurrencyAnalyzer;
+import service.ExchangeRateService;
+import service.LoggingService;
+import service.UserSettingsService;
+
+import java.util.List;
+
+@RestController
+    @RequestMapping("/api")
+    public class RatesController{
+    @Autowired
+    private ExchangeRateService exchangeRateService;
+    private final CurrencyAnalyzer currencyAnalyzer = new CurrencyAnalyzer();
+    @Autowired
+    private UserSettingsService userSettingsService;
+    @Autowired
+    private LoggingService loggingService;
+
+    @GetMapping("/rates")
+    public ExchangeRateResponse getRates(
+            @RequestParam String base,
+            @RequestParam List<String> currencies){
+        try{
+            UserSettings settings = new UserSettings(base, currencies);
+            return exchangeRateService.getCurrentRates(settings);
+        } catch (Exception e) {
+            loggingService.logError("Chyba v /api/rates ",e);
+            throw e;
+        }
+    }
+
+}
