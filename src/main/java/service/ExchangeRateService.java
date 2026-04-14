7ba0bd14cf4b4d60bebaa56c0ce97e26aad707b9
Matous-TUL <matymartinek@seznam.cz>
2026-04-14 15:10:07 +0200

    API call service

--- src/main/java/service/ExchangeRateService.java
+++ src/main/java/service/ExchangeRateService.java
@@ -1,8 +1,9 @@
 package service;
 
-
 import model.ExchangeRateResponse;
+import model.ExchangeRateResponseDate;
 import model.UserSettings;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,6 +22,17 @@ public class ExchangeRateService {
         }
         return response;
     }
-
-
+    public ExchangeRateResponseDate getCurrentRatesDates(UserSettings settings, String dateFrom, String dateTo){
+        String url = "https://api.frankfurter.app/"
+                +dateFrom + ".."+dateTo
+                +"?base="
+                +settings.getBaseCurrency()
+                +"&symbols="
+                +String.join(",", settings.getSelectedCurrencies());
+        ExchangeRateResponseDate response = restTemplate.getForObject(url, ExchangeRateResponseDate.class);
+        if (response == null || response.getRates() == null) {
+            throw new RuntimeException("Nepodařilo se načíst kurzy z API");
+        }
+        return response;
+    }
 }
